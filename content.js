bf719c4edb1a95dc15f57f83e02733397616e096
VOIDRLM <>
2023-04-09 08:53:52 +0530

    Added get and set of dark value data in local storage

--- content.js
+++ content.js
@@ -9,6 +9,21 @@ function setTheme(parameter) {
     var domain = window.location.hostname.replace('www.', '');
     let notRequiredDomains = ['apple.com', 'youtube.com', 'instagram.com', 'bing.com', 'localhost'];
     if (!notRequiredDomains.includes(domain)) {
+        let current = JSON.parse(localStorage.getItem('darkModeData'));
+        console.log(current);
+        if (parameter === 'clicked') {
+            if (current === null || !current) {
+                localStorage.setItem('darkModeData', JSON.stringify({}));
+                current = {};
+            }
+            if (Object.keys(current).length === 0) {
+                let data = { domain: domain, isDark: true };
+                localStorage.setItem('darkModeData', JSON.stringify(data));
+            } else {
+                current.isDark = !current.isDark;
+                localStorage.setItem('darkModeData', JSON.stringify(current));
+            }
+        }
     }
 }
 function mapTheme(parameter, theme) {
